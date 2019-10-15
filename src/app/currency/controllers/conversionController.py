ef4d9a00b66a701e5c448828cc33826e03ae31d8
Samuel Martín <smartin@verbio.com>
2019-10-15 16:47:18 +0200

    Connected controller with serializer

--- src/app/currency/controllers/conversionController.py
+++ src/app/currency/controllers/conversionController.py
@@ -1,9 +1,20 @@
 from aiohttp import web
 from aiohttp_swagger import swagger_path
+from app.currency.serializers.conversionSerializer import ConversionSerializer
+from schematics.exceptions import DataError
 
 
 class ConversionController(web.View):
     @swagger_path("swagger/convert_currency.yaml")
     async def get(self):
-        urlParams = dict(self.request.rel_url.query)
-        return web.Response(text="Not implemented")
+        try:
+            urlParams = dict(self.request.rel_url.query)
+            conversionSerializer = ConversionSerializer(urlParams)
+            conversionSerializer.validate()
+            return web.Response(text="Not implemented")
+        except DataError as e:
+            validationErrors = {
+                key: [error.summary for error in e.messages[key].errors]
+                for key in e.messages.keys()
+            }
+            return web.json_response({"errors": validationErrors}, status=400)
