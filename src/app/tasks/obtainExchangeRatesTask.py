ea7c3c2f6133eb1032f1463d8f0cd91f7d413bfa
Samuel Martín <smartin@verbio.com>
2019-10-20 22:55:20 +0200

    Check if OpenExchangeRates app id  exist before task start

--- src/app/tasks/obtainExchangeRatesTask.py
+++ src/app/tasks/obtainExchangeRatesTask.py
@@ -4,7 +4,10 @@ from app.use_cases.updateCurrencyExchanges import UpdateCurrencyExchanges
 
 
 def obtainExchangeRatesTask():
-    logger = getAppLogger(settings.CELERY_WORKER_NAME)
-    logger.info("Starting celery task to obtain latest exchange rates")
-    UpdateCurrencyExchanges(logger).update()
-    logger.info("Finished celery task to obtain latest exchange rates")
+    if settings.OER_APP_ID:
+        logger = getAppLogger(settings.CELERY_WORKER_NAME)
+        logger.info("Starting celery task to obtain latest exchange rates")
+        UpdateCurrencyExchanges(logger).update()
+        logger.info("Finished celery task to obtain latest exchange rates")
+    else:
+        logger.critical("OpenExchangeRates app id must be provided.")
