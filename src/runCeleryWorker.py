cc60a28e12931c3caea66b3774d1aad6aaa1cf2f
Samuel Martín <smartin@verbio.com>
2019-10-20 14:21:26 +0200

    Set celery task once per day

--- src/runCeleryWorker.py
+++ src/runCeleryWorker.py
@@ -4,4 +4,11 @@ from app.tasks.obtainExchangeRatesTask import obtainExchangeRatesTask
 
 @celeryApp.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
-    sender.add_periodic_task(5.0, celeryApp.task(obtainExchangeRatesTask).s(), name="")
+    sender.add_periodic_task(
+        crontab(minute=0, hour=0),
+        celeryApp.task(obtainExchangeRatesTask).s(),
+        name="Obtain exchange rates every day",
+    )
+
+
+obtainExchangeRatesTask()
