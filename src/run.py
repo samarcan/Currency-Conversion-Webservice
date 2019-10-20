9390d900e0279a0eb4d61e903c4087be56e0bca5
Samuel Martín <smartin@verbio.com>
2019-10-20 17:19:15 +0200

    Sets some settings variables as environments variables

--- src/run.py
+++ src/run.py
@@ -1,5 +1,7 @@
+import settings
 from aiohttp import web
 from app import app
 
 if __name__ == "__main__":
-    web.run_app(app)
+    web.run_app(app, port=settings.SERVER_PORT)
+    web.run_app
--- src/settings.py
+++ src/settings.py
@@ -1,11 +1,12 @@
 import os
 
+SERVER_PORT = os.environ.get("SERVER_PORT", 8080)
 ALLOWED_CURRENCIES = ["EUR", "USD", "PLN", "CZK"]
 BASE_CURRENCY = "USD"
 
 # Redis
-REDIS_HOST = "localhost"
-REDIS_PORT = 6379
+REDIS_HOST = os.environ.get("REDIS_HOST", "localhost")
+REDIS_PORT = os.environ.get("REDIS_PORT", 6379)
 
 # OpenExchangeRate
 OER_URL = "https://openexchangerates.org/api"
